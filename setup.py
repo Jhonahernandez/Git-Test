864bb00572257a504a8ab265270a4526270b4064
jhonahernandez <jhonahernandezc@gmail.com>
2020-09-07 15:06:51 -0500

    My First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'NoStrError',
+    packages = ['package'],
+    version = '0.1',
+    description = 'This package allows to validate errors of type NoStrError',
+    author = 'Alexander Castellanos',
+    author = 'acastll1508@gmail.com',
+    url = ''
+    keyword = ['error', 'str', 'NoStrError']
+)
